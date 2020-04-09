ebb0157017c18d966e3085ef5d00a16cee997ff8
Adolfo <adolfoaguirrecontreras@gmail.com>
2020-04-09 18:50:31 -0400

    Initial commit

---
+++ Assets/Scripts/Menu Controllers/GameplayController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pausePanel;
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pausePanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pausePanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Application.LoadLevel("MainMenu");
+    }
+}
